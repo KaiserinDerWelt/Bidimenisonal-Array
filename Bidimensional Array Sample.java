11ba6344bd587b358d3d138e6946f60b399318a3
Kaiserin_der_Welt <42034666+KaiserinDerWelt@users.noreply.github.com>
2018-11-20 16:07:44 +0100

    Create Bidimensional Array Sample.java

---
+++ Bidimensional Array Sample.java
@@ -0,0 +1,18 @@
+
+
+
+public static void main(String[] args) {  
+      // TODO code application logic here
+          int [][] numeros;
+          int i, j;
+          
+          numeros = new int [2][3];
+          for (i = 0; i < numeros.length; i++)
+              {
+               for (j = 0; j < numeros [i].length; j++)
+                    {
+                     System.out.print(numeros[i][j] + " ");
+                   }
+                System.out.print( "\n");
+                }
+                }
